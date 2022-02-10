241c70157cc02705210bd1a94ac80d5b1733fd1a
GorrettyOkumbe <94072407+GorrettyOkumbe@users.noreply.github.com>
2022-02-10 15:03:37 +0300

    Update dido2.py
    still on love matters

--- dido2.py
+++ dido2.py
@@ -1,1 +1,4 @@
 print("hello_world_images")
+#How best can  I put it,you think you've found love only to realise it is all fun and game for them.
+#Your ex show up at this tym but because you found new love you push them away...huh then boom heart break
+#The tym you need them back(ex)he has long moved on.....just where do broken hearts go!!
